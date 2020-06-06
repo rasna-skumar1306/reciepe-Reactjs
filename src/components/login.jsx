8e9d78d092ce07c355ad34f6408bae014452131a
rasna-skumar1306 <rasna5202@gmail.com>
2020-06-06 12:03:22 +0530

    added register link

--- src/components/login.jsx
+++ src/components/login.jsx
@@ -1,12 +1,21 @@
-import React from "react";
-import Input from "./input";
+import React, { useState } from "react";
+import Register from "./register";
+import Logo from "./logo";
 
 function Login() {
+    const [name,setName] = useState(""); 
+
     return (
-    <div className="content">
-        <h1 className="hello">Hello There!</h1>
-        <Input />
-    </div>);
+        <div>
+        <Logo/>
+        <div className="content">
+            <h1 className="hello">Hello {name} </h1>
+            <input class="user top" type="text" placeholder="Enter your name" value={name} onChange={(e)=>{setName(e.target.value)}}/>
+            <input class="user bottom" type="password" placeholder="Enter your password" />
+            <button class="btn btn-lg btn-success" type="submit" onSubmit={(e)=>{e.preventDefault()}} >Submit</button>
+            <Register />
+        </div>
+        </div>);
 }
 
 export default Login;
