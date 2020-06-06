9b5324eef9117925a1e52655268fe3922aee6818
rasna-skumar1306 <rasna5202@gmail.com>
2020-06-06 14:06:08 +0530

    logo component created

--- src/components/logo.jsx
+++ src/components/logo.jsx
@@ -1,9 +1,9 @@
 import React from "react";
-
+import "bootstrap/dist/css/bootstrap-grid.min.css"
 export default function Logo() {
     return (
         <div className="logo">
-            <h3>Recipe App</h3>
+            <h3 className="logohead">Recipe App</h3>
             <p>from CrazyRas</p>
         </div>);
 }
