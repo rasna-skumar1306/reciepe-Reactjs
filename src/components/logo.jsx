0e76f663ba91f79c1bfad60fe7c642e2f4645937
rasna-skumar1306 <rasna5202@gmail.com>
2020-06-06 12:02:44 +0530

    added logo class and deleted the input class

---
+++ src/components/logo.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Logo() {
+    return (
+        <div className="logo">
+            <h3>Recipe App</h3>
+            <p>from CrazyRas</p>
+        </div>);
+}
