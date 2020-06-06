20812bd6f3f219f446ea4ab2b4c29f0bbeb27dca
rasna-skumar1306 <rasna5202@gmail.com>
2020-06-06 15:55:42 +0530

    first commit

--- src/components/logo.jsx
+++ src/components/logo.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "bootstrap/dist/css/bootstrap-grid.min.css"
 export default function Logo() {
+    
     return (
         <div className="logo">
             <h3 className="logohead">Recipe App</h3>
